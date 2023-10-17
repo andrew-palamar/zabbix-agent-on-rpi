38e0f7673df7c10e0605806d7bae9311a1d6ae29
Паламарчук Андрей <palamarchuk@satel.org>
2023-10-17 12:40:07 +0300

    Installation script checks.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 checkexit() {
     # Check exitcode
     if [ $? != 0 ]; then
-        printf "Installation failed! Process returned bad exitcode."
+        printf "Installation failed! Process returned bad exitcode.\n"
         exit 1
     fi
 }
@@ -12,13 +12,15 @@ sudo apt update
 sudo apt install zabbix-agent2 -y
 
 # Create script location:
-sudo mkdir -p /etc/zabbix/scripts
+scriptpath="/etc/zabbix/scripts"
+[ -d $scriptpath ] || sudo mkdir -p $scriptpath
 
 # Fetch script
-sudo wget https://raw.githubusercontent.com/andrew-palamar/zabbix-agent-on-rpi/ds18b20/raspberrypi.sh -O /etc/zabbix/scripts/raspberrypi.sh
+script="/etc/zabbix/scripts/raspberrypi.sh"
+[ -f $script ] || sudo wget https://raw.githubusercontent.com/andrew-palamar/zabbix-agent-on-rpi/ds18b20/raspberrypi.sh -O $script
 checkexit
 # Change permissions for script:
-sudo chmod 755 /etc/zabbix/scripts/raspberrypi.sh
+sudo chmod 755 $script
 checkexit
 
 # Add zabbix user to video group to get required permissions.
@@ -26,16 +28,25 @@ sudo usermod -aG video zabbix
 checkexit
 
 # Test script as user zabbix:
-printf "Test installed script as zabbix user."
+printf "Test installed script as zabbix user.\n"
 sudo -u zabbix /etc/zabbix/scripts/raspberrypi.sh temperature
 checkexit
+printf "OK\n"
 
 # Add script to zabbix configuration file (append)
-echo 'UserParameter=raspberrypi.sh[*],/etc/zabbix/scripts/raspberrypi.sh $1' | sudo tee -a /etc/zabbix/zabbix_agent2.conf
+string='UserParameter=raspberrypi.sh[*],/etc/zabbix/scripts/raspberrypi.sh $1'
+file='/etc/zabbix/zabbix_agent2.conf'
+
+if grep -qF "$string" "$file"; then
+    echo "String already exists in the destination file."
+else
+    echo "$string" | sudo tee -a "$file"
+fi
+
 checkexit
 
 # Restart the zabbix agent:
-sudo systemctl zabbix-agent restart
-sudo systemctl zabbix-agent status
+sudo systemctl restart zabbix-agent
+sudo systemctl status zabbix-agent
 checkexit
-printf "Installation script completed!"
+printf "Installation script completed!\n"
